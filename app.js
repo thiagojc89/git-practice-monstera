5d972e6a08b5a2915b421e56f139caf5d2ba4ba2
thiago <thiago.cavalcante@generalassemb.ly>
2021-04-13 19:24:14 -0400

    mutl method is working

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
 const calculator = {
 	add: (num1, num2)=> num1 + num2,
-	sub: (num1, num2)=> num1 - num2
+	sub: (num1, num2)=> num1 - num2,
+	mult: (num1, num2)=> num1 * num2,
 }
 
 
 
 console.log(calculator.add(5,3))
 console.log(calculator.sub(5,3))
+console.log(calculator.mult(5,3))
