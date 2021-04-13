8be396f34b12f16771a22de504990d5e5a0f5688
thiago <thiago.cavalcante@generalassemb.ly>
2021-04-13 19:03:48 -0400

    sub method is working

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 const calculator = {
-	add:(num1, num2)=> num1 + num2
+	add: (num1, num2)=> num1 + num2,
+	sub: (num1, num2)=> num1 - num2
 }
 
 
 
 console.log(calculator.add(5,3))
+console.log(calculator.sub(5,3))
